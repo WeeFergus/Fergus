8fc3437a9869cbce15c82bd0f1de20aaa374a46b
Peter Hudack <hudack@towermarketing.net>
2018-11-02 09:16:08 -0400

    fixed mobile hover

--- js/burgertime.js
+++ js/burgertime.js
@@ -1,34 +1,30 @@
-(function(jQuery) { // Begin jQuery
-  jQuery(function() { // DOM ready
+(function(jQuery) {
+  jQuery(function() {
+
     // If a link has a dropdown, add sub menu toggle.
     jQuery('nav ul li a:not(:only-child)').click(function(e) {
-      jQuery(this).siblings('.sub-menu').toggle();
-      
-      // Close one dropdown when selecting another
-      jQuery('.sub-menu').not(jQuery(this).siblings()).hide();
-      e.stopPropagation();
+    	jQuery(this).siblings('.sub-menu').toggle();
+
+    // Close one dropdown when selecting another
+    	jQuery('.sub-menu').not(jQuery(this).siblings()).hide();
+		e.stopPropagation();
     });
-    
-    // Clicking away from dropdown will remove the dropdown class
-    jQuery('html').click(function() {
-      jQuery('.sub-menu').hide();
-    });
-    
-    // Toggle open and close nav styles on click
+
+    // Open and close mobile nav on burger click
     jQuery('#nav-toggle').click(function() {
       jQuery('nav ul').slideToggle();
     });
-    
-    // Hamburger to X toggle
+
+    // Hamburger to X animation
     jQuery('#nav-toggle').on('click', function() {
       this.classList.toggle('active');
     });
-    
+
     // Removes inline style on main nav to make menu reappear on window resize
     jQuery(window).resize(function(){
     if (jQuery(window).width() >= 768) {  //This breakpoint MUST match the mobile breakpoint
 		jQuery('nav#site-navigation ul').removeAttr('style');
-     	}     
+     	}
 	});
   }); // end DOM ready
 })(jQuery); // end jQuery
