c5dc950ab7135e4b6d4ab05e6a967c072beef6fc
Peter Hudack <hudack@towermarketing.net>
2018-11-02 10:57:29 -0400

    cleaned up code

--- page-templates/accordion.php
+++ page-templates/accordion.php
@@ -4,7 +4,7 @@
  *
  * @package fergus
  */
- 
+
 get_header(); ?>
 
 <main>
@@ -12,14 +12,14 @@ get_header(); ?>
 		<?php
 			if( have_rows('accordion') ): ?>
 				<div id="accordion">
-			 	 	<?php while ( have_rows('accordion') ) : the_row(); ?>
-			 	 		<div class="accordion">
-				 	 		<a class="accordion-section-title" href="#"><?php the_sub_field('section_title'); ?></a>
-			 	 			<div class="accordion-section-content">
-			 	 				<?php the_sub_field('sub_item'); ?>
-			 	 			</div>	
-				 	 	</div> 
-				 	<?php endwhile;
+					<?php while ( have_rows('accordion') ) : the_row(); ?>
+						<div class="accordion">
+							<a class="accordion-section-title" href="#"><?php the_sub_field('section_title'); ?></a>
+							<div class="accordion-section-content">
+								<?php the_sub_field('sub_item'); ?>
+							</div>
+						</div>
+					<?php endwhile;
 				else :
 			endif;
 		?>
